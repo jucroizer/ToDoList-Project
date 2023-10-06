80b46289732d93757c18b4fd3213bc123a8271eb
jucroizer <81417866+jucroizer@users.noreply.github.com>
2023-10-06 12:40:04 +0200

    realisation du front avec tous les elements necessaires

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 function App() {
  
   return (
-    <>
-     <h1 className="bg-red-400 text-3xl py-16">ToDoList Project</h1>
-    </>
+    <div className="h-screen bg-slate-900">
+      <div className="max-w-4xl mx-auto pt-20 px-6">
+        <h1 className="text-3xl text-slate-100 mb-4">To-do List</h1>
+      
+        <form className="mb-10"> 
+          <label htmlFor="todo-item"
+          className="text-slate-50">
+            Ajouter une chose à faire
+          </label>
+          <input 
+          type="text"
+          className="mt-1 block w-full rounded"
+          />
+
+          <button className="mt-4 py-2 px-2 bg-slate-50 rounded min-w-[115px]">Ajouter</button>
+        </form>
+
+        <ul>
+          
+        </ul>
+      </div>
+    </div>
   )
 }
 
