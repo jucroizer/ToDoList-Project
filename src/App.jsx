573cac3a4a58ab2ef48ff6449893fcd0f6054ccf
jucroizer <81417866+jucroizer@users.noreply.github.com>
2023-10-06 13:41:25 +0200

    fin du projet, conception de l'ajout de taches et validation du formulaire

--- src/App.jsx
+++ src/App.jsx
@@ -18,31 +18,59 @@ function App() {
       content: "item 3"
     }
   ])
+  const [toDo, setToDo] = useState("")
+  const [showValidation, setShowValidation] = useState(false)
 
   function deleteToDo(id){
     setToDoList(toDoList.filter(todo => todo.id !== id))
   }
 
+
+  function handleSubmit(e){
+    e.preventDefault()
+
+    if(toDo === ""){
+      setShowValidation(true)
+      return
+    }
+
+    setToDoList([...toDoList, {id: nanoid(4), content: toDo}])
+    setToDo("")
+    setShowValidation(false)
+  }
+
   return (
-    <div className="h-screen bg-slate-900">
+    <div className="h-screen">
       <div className="max-w-4xl mx-auto pt-20 px-6">
         <h1 className="text-3xl text-slate-100 mb-4">To-do List</h1>
       
-        <form className="mb-10"> 
+        <form onSubmit={handleSubmit} className="mb-10"> 
           <label htmlFor="todo-item"
           className="text-slate-50">
-            Ajouter une chose à faire
+            Ajouter une tâche
           </label>
-          <input 
+          <input
+          value={toDo}
+          onChange={e => setToDo(e.target.value)}
           type="text"
           className="mt-1 block w-full rounded"
           />
+          {showValidation && (
+            <p className="text-red-200">
+              Ajoutez d'abord du contenu à votre tâche
+            </p>
+          )}
 
           <button className="mt-4 py-2 px-2 bg-slate-50 rounded min-w-[115px]">Ajouter</button>
         </form>
 
         <ul>
-          {toDoList.map(item => (
+          {toDoList.length === 0 && (
+            <li className="text-slate-50 text-md">
+              Pas d'items à afficher...
+            </li>
+          )}
+          {toDoList.length > 0 && toDoList.map(item => (
             <ListItem key={item.id} itemData={item} deleteToDo={deleteToDo}/>
           ))}
           
